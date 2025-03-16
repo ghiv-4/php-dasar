6b17b7013547a1b103d51cab29d8506bfed852bb
Ghivary <rais.alghivary4@gmail.com>
2025-03-16 09:08:04 +0700

    menambahkan fungsi ubah.php

---
+++ pertemuan11/hapus.php
@@ -0,0 +1,23 @@
+<?php 
+require 'functions.php';
+// tangkap dulu id yang ada di index.php nya
+$id = $_GET["id"];
+
+// buat logika hapus
+if (hapus ($id)>0) {
+    echo "
+        <script>
+            alert('data BERHASIL dihapus !');
+            document.location.href = 'index.php';
+        </script>
+    ";
+} else {
+    echo "
+    <script>
+        alert('data GAGAL dihapus !');
+        document.location.href = 'index.php';
+    </script>
+    ";
+}
+
+?>
